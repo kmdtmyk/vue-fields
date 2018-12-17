917cba34b1fa8cfaa14b8b712d02dc4cefd32347
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-12-17 21:33:14 +0900

    Install addon knobs

--- stories/TextField.stories.js
+++ stories/TextField.stories.js
@@ -1,21 +1,32 @@
 
 import {storiesOf} from '@storybook/vue'
 import VueInfoAddon from 'storybook-addon-vue-info'
+import {withKnobs, text, boolean} from '@storybook/addon-knobs'
 
 import TextField from '../src/components/TextField'
 
-storiesOf('TextField', module).addDecorator(VueInfoAddon)
-  .add('basic', () => ({
-    components: { TextField },
-    template: `
-      <div>
-        <text-field v-model='value'/>{{value}}
-      </div>
-    `,
-    data(){
-      return {value: ''}
-    },
-  }))
+storiesOf('TextField', module)
+  .addDecorator(VueInfoAddon)
+  .addDecorator(withKnobs)
+  .add('basic', () => {
+    const value = text('value', 'foo')
+    const readonly = boolean('readonly', false)
+    const disabled = boolean('disabled', false)
+    return {
+      components: { TextField },
+      template: `
+        <div>
+          <text-field v-model='value' :disabled='${disabled}' :readonly='${readonly}'/>
+          {{value}}
+        </div>
+      `,
+      data(){
+        return {
+          value,
+        }
+      },
+    }
+  })
   .add('multiple', () => ({
     components: { TextField },
     template: `
