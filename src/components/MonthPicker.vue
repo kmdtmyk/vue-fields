8313c0dc81508e79a78dbdd9756260d2d80ca629
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-12-27 15:04:55 +0900

    Add footer buttons to month picker

--- src/components/MonthPicker.vue
+++ src/components/MonthPicker.vue
@@ -8,7 +8,10 @@
     tbody
       tr(v-for='(_, y) in 4')
         td(v-for='(_, x) in 3')
-          button(type='button' @click='select(x + y * 3 + 1)') {{x + y * 3 + 1}}月
+          button(type='button' @click='select(year, x + y * 3 + 1)') {{x + y * 3 + 1}}月
+  footer
+    button(type='button' @click='currentMonth' tabindex='-1') 今月
+    button(type='button' @click='clear' tabindex='-1') クリア
 </template>
 
 <script>
@@ -39,50 +42,65 @@ export default {
         this.previousYear()
       }
     },
-    select(month){
-      const date = new Midnight(this.year, month)
+    select(year, month){
+      const date = new Midnight(year, month)
       this.$emit('input', dateformat(date, 'yyyy-mm'))
+    },
+    currentMonth(){
+      const today = new Midnight()
+      this.select(today.year(), today.month())
+    },
+    clear(){
+      this.$emit('input', null)
     },
   },
 }
 </script>
 
 <style lang='scss' scoped>
-$border-color: #ced4da;
-
 .month-picker{
+  $border-color: #ced4da;
+
   background-color: white;
   border: 1px solid $border-color;
-}
 
-header{
-  display: flex;
-  justify-content: space-between;
-}
+  header{
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    justify-content: space-between;
+  }
 
-table{
-  border-collapse: collapse;
-  td{
-    padding: 0;
-    width: 3em;
-    button{
-      text-align: center;
-      border: 0;
-      background: none;
-      color: inherit;
-      width: 100%;
-      &:hover{
-        background: #bcd4fd;
+  footer{
+    border-top: 1px solid $border-color;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  table{
+    border-collapse: collapse;
+    td{
+      padding: 0;
+      width: 3em;
+      button{
+        text-align: center;
+        border: 0;
+        background: none;
+        color: inherit;
+        width: 100%;
+        &:hover{
+          background: #bcd4fd;
+        }
       }
     }
   }
-}
 
-button{
-  height: 2em;
-  &:hover{
-    cursor: pointer;
+  button{
+    height: 2em;
+    &:hover{
+      cursor: pointer;
+    }
   }
+
 }
 </style>
 
