94b6c2ce84e23b09119900e32cbb7b0f950fcc8c
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-01-29 11:18:03 +0900

    Add scroll animation to month picker

--- src/components/MonthPicker.vue
+++ src/components/MonthPicker.vue
@@ -7,10 +7,10 @@
       button(type='button' @click='nextYear' tabindex='-1') &gt;
     div
       button(type='button' @click='currentYear' tabindex='-1') 今年
-  .picker
+  .picker(:class='animation')
     table
-      tbody
-        tr(v-for='(_, y) in 3')
+      transition-group(tag='tbody' @beforeEnter='transitionBeforeEnter' @afterLeave='transitionAfterLeave')
+        tr(v-for='(_, y) in 3' :key='`${year}:${y}`')
           td(v-for='(_, x) in 4')
             button(type='button' @click='select(year, x + y * 4 + 1)') {{x + y * 3 + 1}}月
   footer
@@ -26,23 +26,48 @@ export default {
   data(){
     const date = new Midnight()
     const year = date.year()
-    const month = date.month()
     return {
       year,
-      month,
+      animationCount: 0,
+      animation: '',
     }
   },
+  computed: {
+    animating(){
+      return 0 < this.animationCount
+    },
+  },
   methods: {
+    transitionBeforeEnter(e){
+      this.animationCount++
+    },
+    transitionAfterLeave(e){
+      this.animationCount--
+      if(this.animationCount == 0){
+        this.animation = ''
+      }
+    },
     previousYear(){
+      if(this.animating){
+        return
+      }
       this.year--
     },
     nextYear(){
+      if(this.animating){
+        return
+      }
       this.year++
     },
     wheel(e){
+      if(this.animating){
+        return
+      }
       if(0 < e.deltaY){
+        this.animation = 'next'
         this.nextYear()
       }else if(e.deltaY < 0){
+        this.animation = 'previous'
         this.previousYear()
       }
     },
@@ -51,6 +76,9 @@ export default {
       this.$emit('input', dateformat(date, 'yyyy-mm'))
     },
     currentYear(){
+      if(this.animating){
+        return
+      }
       const today = new Midnight()
       this.year = today.year()
     },
@@ -72,7 +100,7 @@ export default {
   background-color: white;
   border: 1px solid $border-color;
 
-  header, footer, .picker{
+  header, footer{
     padding: 2px;
   }
 
@@ -91,22 +119,67 @@ export default {
     justify-content: space-between;
   }
 
-  table{
-    border-collapse: collapse;
-    td{
-      padding: 0;
-      width: 3em;
-      button{
-        text-align: center;
-        border: 0;
-        background: none;
-        color: inherit;
-        width: 100%;
-        &:hover{
-          background: #bcd4fd;
+  .picker{
+    $row-height: 32px;
+    height: $row-height * 3;
+    overflow: hidden;
+
+    &.previous, &.next{
+      .v-enter-active, .v-leave-active {
+        transition: transform 0.2s;
+        transition-timing-function: linear;
+      }
+    }
+
+    &.next{
+      .v-enter {
+        transform: translateY(0);
+      }
+      .v-enter-to{
+        transform: translateY($row-height * -3);
+      }
+      .v-leave{
+        transform: translateY(0);
+      }
+      .v-leave-to{
+        transform: translateY($row-height * -3);
+      }
+    }
+
+    &.previous{
+      .v-enter {
+        transform: translateY($row-height * -3 * 2);
+      }
+      .v-enter-to{
+        transform: translateY($row-height * -3);
+      }
+      .v-leave{
+        transform: translateY(0);
+      }
+      .v-leave-to{
+        transform: translateY($row-height * 3);
+      }
+    }
+
+    table{
+      border-collapse: collapse;
+      td{
+        padding: 0;
+        width: 3em;
+        button{
+          height: $row-height;
+          text-align: center;
+          border: 0;
+          background: none;
+          color: inherit;
+          width: 100%;
+          &:hover{
+            background: #bcd4fd;
+          }
         }
       }
     }
+
   }
 
   button{
