cc54273076536922309eaca15ca79a59d06ffd24
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-04-08 14:35:16 +0900

    Add option precision to format number method

--- src/components/lib/NumberUtil.js
+++ src/components/lib/NumberUtil.js
@@ -2,15 +2,24 @@ import Parser from './Parser'
 
 export default class{
 
-  static format(value){
+  static format(value, options = {}){
     const number = Parser.parseFloat(value)
     if(number == null){
       return null
     }
 
-    const [integer, decimal] = number.toString().split('.').map(it => parseInt(it))
+    const {integer, decimal} = split(number)
     if(!decimal){
       return integer.toLocaleString()
+    }
+
+    const {precision} = options
+    if(precision != null){
+      const {integer, decimal} = split(round(number, precision))
+      if(decimal == null){
+        return integer.toLocaleString()
+      }
+      return `${integer.toLocaleString()}.${decimal}`
     }
 
     return `${integer.toLocaleString()}.${decimal}`
@@ -18,3 +27,13 @@ export default class{
 
 }
 
+const split = (number) => {
+  const [integer, decimal] = number.toString()
+    .split('.')
+    .map(it => it ? parseInt(it) : it)
+  return {integer, decimal}
+}
+
+const round = (number, precision) => {
+  return parseFloat(number.toFixed(precision))
+}
--- src/components/lib/NumberUtil.test.js
+++ src/components/lib/NumberUtil.test.js
@@ -2,8 +2,8 @@ import NumberUtil from './NumberUtil'
 
 describe('format', () => {
 
-  function subject(value){
-    return NumberUtil.format(value)
+  function subject(value, options){
+    return NumberUtil.format(value, options)
   }
 
   it('number', () => {
@@ -14,6 +14,12 @@ describe('format', () => {
   it('string', () => {
     expect(subject('123456789')).toBe('123,456,789')
     expect(subject('not a number')).toBeNull()
+  })
+
+  it('precision', () => {
+    expect(subject(0.124, {precision: 2})).toBe('0.12')
+    expect(subject(0.125, {precision: 2})).toBe('0.13')
+    expect(subject(123.45, {precision: 0})).toBe('123')
   })
 
   it('null', () => {
