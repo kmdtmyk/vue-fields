bf7fe39a7b9722116b0078112e4418317b47620e
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-06-06 13:33:22 +0900

    Install addon-storysource

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,5 +1,6 @@
 module.exports = (storybookBaseConfig, configType, defaultConfig) => {
 
+
   defaultConfig.module.rules.push(
     {
       test: /\.pug$/,
@@ -9,11 +10,16 @@ module.exports = (storybookBaseConfig, configType, defaultConfig) => {
       test: /\.scss$/,
       loader: ['style-loader', 'css-loader', 'sass-loader']
     },
+    {
+      test: /\.stories\.jsx?$/,
+      loaders: [require.resolve('@storybook/addon-storysource/loader')],
+      enforce: 'pre',
+    },
     // {
     //   test: /\.vue$/,
     //   loader: 'storybook-addon-vue-info/loader',
     //   enforce: 'post'
-    // }
+    // },
   )
 
   return defaultConfig
