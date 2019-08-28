8158cfe5e802cd7dd5ea1849896552c8a2c20211
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-08-28 10:39:10 +0900

    Hide wip storybook in production mode

--- src/stories/TimeField.stories.js
+++ src/stories/TimeField.stories.js
@@ -3,41 +3,48 @@ import {withKnobs, text, number, boolean, color} from '@storybook/addon-knobs'
 
 import TimeField from '../components/TimeField'
 
-storiesOf('TimeField (WIP)', module)
-  .addDecorator(withKnobs)
-  .add('basic', () => {
-    const value = text('value', '')
-    const size = number('size', 10)
-    const readonly = boolean('readonly', false)
-    const disabled = boolean('disabled', false)
-    return {
-      components: {TimeField},
-      template: `
-        <div>
-          <time-field v-model='value' :size='${size}' :disabled='${disabled}' :readonly='${readonly}'/>
-          {{value}}
-        </div>
-      `,
-      data(){
-        return {value}
-      },
-    }
-  }, {info: true})
-  .add('style', () => {
-    const value = text('value', '')
-    const size = number('size', 24)
-    const colour = color('color', 'red')
-    return {
-      components: {TimeField},
-      template: `
-        <div>
-          <time-field v-model='value' style='font-size: ${size}px; color: ${colour};'/>
-          {{value}}
-        </div>
-      `,
-      data(){
-        return {value}
-      },
-    }
-  }, {info: true})
+(() => {
 
+  if(process.env.NODE_ENV !== 'development'){
+    return
+  }
+
+  storiesOf('TimeField (WIP)', module)
+    .addDecorator(withKnobs)
+    .add('basic', () => {
+      const value = text('value', '')
+      const size = number('size', 10)
+      const readonly = boolean('readonly', false)
+      const disabled = boolean('disabled', false)
+      return {
+        components: {TimeField},
+        template: `
+          <div>
+            <time-field v-model='value' :size='${size}' :disabled='${disabled}' :readonly='${readonly}'/>
+            {{value}}
+          </div>
+        `,
+        data(){
+          return {value}
+        },
+      }
+    }, {info: true})
+    .add('style', () => {
+      const value = text('value', '')
+      const size = number('size', 24)
+      const colour = color('color', 'red')
+      return {
+        components: {TimeField},
+        template: `
+          <div>
+            <time-field v-model='value' style='font-size: ${size}px; color: ${colour};'/>
+            {{value}}
+          </div>
+        `,
+        data(){
+          return {value}
+        },
+      }
+    }, {info: true})
+
+})()
