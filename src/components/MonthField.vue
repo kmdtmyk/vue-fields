ec5cb1bd3dc472196f1e92f1bf6073a927e76828
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-12-13 11:02:34 +0900

    Fix box-shadow

--- src/components/MonthField.vue
+++ src/components/MonthField.vue
@@ -107,7 +107,7 @@ export default {
   .month-picker{
     position: absolute;
     z-index: 1;
-    box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
   }
 }
 </style>
