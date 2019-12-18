9d99a9a10af27ca6a65c45dc8dbf0792f8c0f77b
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-12-18 17:32:34 +0900

    Fix dropdown list item style

--- src/components/DropdownListItem.vue
+++ src/components/DropdownListItem.vue
@@ -1,6 +1,7 @@
 <template lang='pug'>
 div(
   class='dropdown-list-item'
+  :class='{text: textSlot}'
   v-on='$listeners'
 )
   slot
@@ -8,19 +9,38 @@ div(
 
 <script>
 export default {
+  data(){
+    return {
+      textSlot: this.$slots.default[0].children == null
+    }
+  },
 }
 </script>
 
 <style lang='scss' scoped>
 .dropdown-list-item{
-  padding: 0.2em 0.4em;
+  $padding: 0.2em 0.4em;
+
+  &.text{
+    padding: $padding;
+  }
+
+  > *{
+    padding: $padding;
+    display: block;
+  }
 
   &:not(.empty){
     cursor: pointer;
   }
 
+  $selected-color: #bedcf5;
+
   &.selected{
-    background-color: #bedcf5;
+    background-color: $selected-color;
+    > * {
+      background-color: $selected-color !important;
+    }
   }
 
 }
