56bbcc87aefa5046eead07128435ab0951588a21
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-03-06 13:38:22 +0900

    Fix integer field test

--- src/components/IntegerField.test.js
+++ src/components/IntegerField.test.js
@@ -3,19 +3,39 @@ import Component from './IntegerField'
 
 describe('name', () => {
 
-  it('has hidden field', () => {
+  it('with props value', () => {
     const wrapper = mount(Component, {
       propsData: {
         name: 'foo',
-        value: '123456789',
-        delimiter: true,
+        value: '123',
       }
     })
     const input = wrapper.find('input[type=text]')
     const hidden = wrapper.find('input[type=hidden]')
     expect(input.attributes().name).toBeUndefined()
     expect(hidden.attributes().name).toBe('foo')
-    expect(hidden.attributes().value).toBe('123456789')
+    expect(hidden.attributes().value).toBe('123')
+    input.setValue('456')
+    expect(hidden.attributes().value).toBe('456')
+    wrapper.setProps({value: '789'})
+    expect(hidden.attributes().value).toBe('789')
+  })
+
+  it('without props value', () => {
+    const wrapper = mount(Component, {
+      propsData: {
+        name: 'foo',
+      }
+    })
+    const input = wrapper.find('input[type=text]')
+    const hidden = wrapper.find('input[type=hidden]')
+    expect(input.attributes().name).toBeUndefined()
+    expect(hidden.attributes().name).toBe('foo')
+    expect(hidden.attributes().value).toBeUndefined()
+    input.setValue('123')
+    expect(hidden.attributes().value).toBe('123')
+    wrapper.setProps({value: '789'})
+    expect(hidden.attributes().value).toBe('789')
   })
 
 })
@@ -46,25 +66,31 @@ describe('keydown', () => {
   it('up', () => {
     const wrapper = mount(Component, {
       propsData: {
+        name: 'foo',
         value: '',
       }
     })
 
-    const input = wrapper.find('input')
+    const input = wrapper.find('input[type=text]')
+    const hidden = wrapper.find('input[type=hidden]')
     input.trigger('keydown.up')
     expect(wrapper.emitted('input')[0]).toEqual([1])
+    expect(hidden.attributes().value).toEqual('1')
   })
 
   it('down', () => {
     const wrapper = mount(Component, {
       propsData: {
+        name: 'foo',
         value: '',
       }
     })
 
-    const input = wrapper.find('input')
+    const input = wrapper.find('input[type=text]')
+    const hidden = wrapper.find('input[type=hidden]')
     input.trigger('keydown.down')
     expect(wrapper.emitted('input')[0]).toEqual([-1])
+    expect(hidden.attributes().value).toEqual('-1')
   })
 
 })
--- src/components/NumberField.test.js
+++ src/components/NumberField.test.js
@@ -3,21 +3,39 @@ import Component from './NumberField'
 
 describe('name', () => {
 
-  it('has hidden field', () => {
+  it('with props value', () => {
     const wrapper = mount(Component, {
       propsData: {
         name: 'foo',
-        value: '123456789',
-        delimiter: true,
+        value: '123',
       }
     })
     const input = wrapper.find('input[type=text]')
     const hidden = wrapper.find('input[type=hidden]')
     expect(input.attributes().name).toBeUndefined()
     expect(hidden.attributes().name).toBe('foo')
-    expect(hidden.attributes().value).toBe('123456789')
+    expect(hidden.attributes().value).toBe('123')
     input.setValue('456')
     expect(hidden.attributes().value).toBe('456')
+    wrapper.setProps({value: '789'})
+    expect(hidden.attributes().value).toBe('789')
+  })
+
+  it('without props value', () => {
+    const wrapper = mount(Component, {
+      propsData: {
+        name: 'foo',
+      }
+    })
+    const input = wrapper.find('input[type=text]')
+    const hidden = wrapper.find('input[type=hidden]')
+    expect(input.attributes().name).toBeUndefined()
+    expect(hidden.attributes().name).toBe('foo')
+    expect(hidden.attributes().value).toBeUndefined()
+    input.setValue('123')
+    expect(hidden.attributes().value).toBe('123')
+    wrapper.setProps({value: '789'})
+    expect(hidden.attributes().value).toBe('789')
   })
 
 })
